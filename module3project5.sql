6ec15fc5007d69ef8592fa77992894996578e6a7
Abhyudya <42513946+Abhyudyabajpai@users.noreply.github.com>
2020-05-09 22:43:20 +0530

    Create module3project5.sql

---
+++ module3project5.sql
@@ -0,0 +1,20 @@
+select * from trips;
+select * from riders;
+select * from cars;
+
+select * from riders cross join cars ;
+
+select * from trips left join  riders on riders.id = trips.rider_id;
+
+select * from trips join cars on trips.car_id = cars.id;
+
+select * from riders 
+union 
+select * from riders2;
+
+select avg(cost) from trips;
+
+select * from riders where total_trips< 500;
+
+select count(status) from cars where status = 'active'; 
+select * from cars order by trips_completed desc limit 2;  
